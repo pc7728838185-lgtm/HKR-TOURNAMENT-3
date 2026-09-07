d214d42c57b9789f399b6394f6263d96054cf853
pc7728838185-lgtm <pc7728838185@gmail.com>
2026-09-07 23:25:27 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,3 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+const config: CapacitorConfig={appId:'com.hkr.tournaments',appName:'HKR Tournaments',webDir:'.',server:{cleartext:false}};
+export default config;
